602f9a3130b433facf5b11b6b306d0f6bfadf17b
Dibyakanta Barik <dibyakanta.barik@daffodilsw.com>
2025-05-13 23:01:07 +0530

    Added test cases for Contact

---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,34 @@
+import Contact from "../Contact";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+//it and test both are same
+describe("Contact us page test cases", () => {
+  it("Should load contact us component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("Should load button inside contact component", () => {
+    render(<Contact />);
+    const button = screen.getByRole("button");
+    // const button = screen.getByText("Submit");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Should load input inside contact component", () => {
+    render(<Contact />);
+    const input = screen.getByPlaceholderText("name");
+
+    expect(input).toBeInTheDocument();
+  });
+});
+
+test("Should load two input inside contact component", () => {
+  render(<Contact />);
+  const inputBoxes = screen.getAllByRole("textbox");
+
+  expect(inputBoxes.length).toBe(2);
+});
